b0d7ececa52beb180440ca340806d8c6d61ec33d
Tiago Porto <tiagoporto@ymail.com>
2016-02-14 12:56:38 -0200

    Fix web-font

---
+++ dist/js/scripts.js
@@ -0,0 +1,7 @@
+/*!	Sign Of The Horns CSS v0.0.1
+*	http://tiagoporto.github.io/sign-of-the-horns-css/
+*	Copyright (c) initialyear-atualyear Author Name (Site)
+*	Released under the MIT license
+*/
+
+// JS
