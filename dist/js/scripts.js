007068d9197552858dc9344eff90820f360d198d
Tiago Porto <tiagoporto@ymail.com>
2016-02-27 20:16:53 -0300

    Fix version

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,6 @@
-/*!	Sign Of The Horns CSS v0.0.1
+/*!	Sign Of The Horns CSS v0.1.0
 *	http://tiagoporto.github.io/sign-of-the-horns-css/
-*	Copyright (c) initialyear-atualyear Author Name (Site)
+*	Copyright (c) 2014-2016 Tiago Porto (http://tiagoporto.com)
 *	Released under the MIT license
 */
 
