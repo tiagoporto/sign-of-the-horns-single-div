823e969f7304304142242eb29d94956bf7405dfe
Tiago Porto <tiago@tiagoporto.com>
2024-02-26 15:43:45 -0300

    Add html minification, compress images and update icons

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
   base: '/sign-of-the-horns-single-div/',
-  root: 'src',
+  root: './src',
   publicDir: '../public',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
   },
+  plugins: [ViteImageOptimizer({}), ViteMinifyPlugin({})],
 })
