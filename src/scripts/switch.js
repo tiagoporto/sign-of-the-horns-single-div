a039708f3dcf46d334839a2a99f6b1ca92015c30
Tiago Porto <tiago@tiagoporto.com>
2024-12-03 22:12:04 -0300

    Replace GTM to GTAG

--- src/scripts/switch.js
+++ src/scripts/switch.js
@@ -1,11 +1,24 @@
+const trackEvent = (type) => {
+  if (process.env.NODE_ENV === 'production' && window.gtag) {
+    window.gtag('event', 'click', {
+      event_category: 'switch',
+      event_label: 'Switch Sign Of The Horns',
+      value: type,
+    })
+  }
+}
+
 document.querySelector('#switch-input').addEventListener('change', (event) => {
   const signOfTheHornsClasses =
     document.querySelector('#sign-of-the-horns').classList
+
   if (event.target.checked) {
     signOfTheHornsClasses.remove('sign-of-the-horns_detailed')
     signOfTheHornsClasses.add('sign-of-the-horns_simplified')
+    trackEvent('simplified')
   } else {
     signOfTheHornsClasses.add('sign-of-the-horns_detailed')
     signOfTheHornsClasses.remove('sign-of-the-horns_simplified')
+    trackEvent('detailed')
   }
 })
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
   base: '/sign-of-the-horns-single-div/',
@@ -10,5 +11,5 @@ export default defineConfig({
     outDir: '../dist',
     emptyOutDir: true,
   },
-  plugins: [ViteImageOptimizer({}), ViteMinifyPlugin({})],
+  plugins: [ViteImageOptimizer({}), ViteMinifyPlugin({}), createHtmlPlugin()],
 })
