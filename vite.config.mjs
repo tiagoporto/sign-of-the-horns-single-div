058215ae2d4f984d75871b5c6912916f7e15f59b
Tiago Porto <tiago@tiagoporto.com>
2025-01-20 15:52:53 -0300

    Add base to vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import { createHtmlPlugin } from 'vite-plugin-html'
 
 export default defineConfig({
+  base: '/sign-of-the-horns-single-div/',
   root: './src',
   publicDir: '../public',
   build: {
