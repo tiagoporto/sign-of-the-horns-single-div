820528d852b3849c404e9d720dcc5f1eeca9b672
Tiago Porto <tiago@tiagoporto.com>
2025-03-17 14:57:34 -0300

    Add clicking track

--- src/scripts/switch.js
+++ src/scripts/switch.js
@@ -1,6 +1,6 @@
 const trackEvent = (type) => {
   if (process.env.NODE_ENV === 'production' && globalThis.gtag) {
-    globalThis.gtag('event', 'switch_sign_of_the_horns', {
+    globalThis.gtag('event', 'sign_of_the_horns', {
       event_label: type,
     })
   }
@@ -13,10 +13,10 @@ document.querySelector('#switch-input').addEventListener('change', (event) => {
   if (event.target.checked) {
     signOfTheHornsClasses.remove('sign-of-the-horns_detailed')
     signOfTheHornsClasses.add('sign-of-the-horns_simplified')
-    trackEvent('simplified')
+    trackEvent('Simplified Version')
   } else {
     signOfTheHornsClasses.add('sign-of-the-horns_detailed')
     signOfTheHornsClasses.remove('sign-of-the-horns_simplified')
-    trackEvent('detailed')
+    trackEvent('Detailed Version')
   }
 })
